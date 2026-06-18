cca92e8b0956552897be957f1f14ab8c1c0273c5
Yousef Esbitan <yousef_espetan@yahoo.com>
2026-06-19 01:22:36 +0300

    coding layouts

---
+++ app/components/layout/TopBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="bg-blue-500">
+        <p class="text-center">Pretty Cool Text Around</p>
+    </div>
+</template>
+
+
+<script setup>
+
+
+</script>
+
+
+<style>
+
+</style>
---
+++ app/layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bg-red-200">
+    <LayoutTopBar/>
+    <LayoutNavBar/>
+
+    <main>
+        <slot />
+    </main>
+
+
+    <LayoutFooter/>
+  </div>
+</template>
+
+<script setup>
+/*
+definePageMeta({
+  layout: 'default'
+})
+*/
+
+</script>
+
+<style>
+
+</style>
