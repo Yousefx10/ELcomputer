813bd32a252cdc4c77893e6a28c0426561a25715
Yousef Esbitan <yousef_espetan@yahoo.com>
2026-06-18 17:56:54 +0300

    configure supabase adding .env will not uploaded to git repo and disable types and redirect

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss'],
+
+  supabase: {
+    types: false, //disable for now TS types
+    redirect:false //disable for now login redirect
+  }
+
 })
