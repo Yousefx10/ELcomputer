8969dce25b7b098168f2d4f55279d0bf527b7de2
Yousef Esbitan <yousef_espetan@yahoo.com>
2026-06-18 18:31:30 +0300

    define pages and hello world tailwind and NuxtPages works also removed nuxtjsTailwindcss from modules

---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p class="text-center text-blue-500">Hello World</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/supabase'],
 
   supabase: {
     types: false, //disable for now TS types
