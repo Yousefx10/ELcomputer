65346579712de27457f6679c9c9ee3bd1994f49a
Yousef Esbitan <yousef_espetan@yahoo.com>
2026-06-20 01:54:37 +0300

    Fixing the TOP CATEGORIES loop and define a 5 data array

--- app/components/cards/TopCategories.vue
+++ app/components/cards/TopCategories.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
-        <p class="text-center font-bold text-4xl">TOP CATEGORIES</p>
+        <p class="text-center font-bold text-4xl pb-5">TOP CATEGORIES</p>
 
         <section class="flex gap-5">
             <!-- writting extra for loop to repeat same data 5 times, temporary only -->
-            <div class="flex flex-col items-center " v-for="n in 5">
-                <div    v-for="offer in offers" :key="n"
-                        class="rounded-[50px] overflow-hidden">
+            <div            class="flex flex-col items-center " 
+                            v-for="offer in offers" :key="offer.id">
+
+                <div        class="rounded-[50px] overflow-hidden">
                     <img    class="w-full h-full object-cover" 
                             :src="offer.image"/>
                 </div>
+
                 <p class="font-bold py-3">{{offer.title}}</p>
+
             </div>
         </section>
 
@@ -24,7 +27,29 @@
             id:1,
             image:'https://placehold.co/200x200',
             title:'Hedseat',
-        }
+        },
+        {
+            id:2,
+            image:'https://placehold.co/200x200',
+            title:'Hedseat',
+        },
+        {
+            id:3,
+            image:'https://placehold.co/200x200',
+            title:'Hedseat',
+        },
+        {
+            id:4,
+            image:'https://placehold.co/200x200',
+            title:'Hedseat',
+        },
+        {
+            id:5,
+            image:'https://placehold.co/200x200',
+            title:'Hedseat',
+        },
+        
+        
     ]
 </script>
 
