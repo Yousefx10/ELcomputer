45b2afc8cdb950f0df4cf25bf3212857ab2fbd46
Yousef Esbitan <yousef_espetan@yahoo.com>
2026-06-19 23:35:32 +0300

    Define Top Categories structure

--- app/components/cards/TopCategories.vue
+++ app/components/cards/TopCategories.vue
@@ -3,12 +3,14 @@
         <p class="text-center font-bold text-4xl">TOP CATEGORIES</p>
 
         <section class="flex gap-5">
-            <div class="flex flex-col items-center ">
-                <div class="rounded-[50px] overflow-hidden">
-                    <img    class=" w-full h-full object-cover" 
-                            src="https://placehold.co/200x200"/>
+            <!-- writting extra for loop to repeat same data 5 times, temporary only -->
+            <div class="flex flex-col items-center " v-for="n in 5">
+                <div    v-for="offer in offers" :key="n"
+                        class="rounded-[50px] overflow-hidden">
+                    <img    class="w-full h-full object-cover" 
+                            :src="offer.image"/>
                 </div>
-                <p class="font-bold py-3">Hedseat</p>
+                <p class="font-bold py-3">{{offer.title}}</p>
             </div>
         </section>
 
@@ -17,7 +19,13 @@
 </template>
 
 <script setup>
-
+    const offers=[
+        {
+            id:1,
+            image:'https://placehold.co/200x200',
+            title:'Hedseat',
+        }
+    ]
 </script>
 
 <style>
