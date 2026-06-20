feb8db82d9fdfbfbf564d891faaf974aa872f222
Yousef Esbitan <yousef_espetan@yahoo.com>
2026-06-20 18:15:47 +0300

    Styles fro Product Section

--- app/components/cards/ProductCard.vue
+++ app/components/cards/ProductCard.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="border-1 border-gray-500 rounded-2xl w-[280px] h-[370px] overflow-hidden">
+    <div class="inline-block bg-white/90 border-1 border-gray-500 rounded-2xl w-[280px] h-[370px] overflow-hidden">
         <p class="text-center m-2">Product Long Title</p>
         <div class="relative">
             <img    class="h-full w-full object-cover"
                     src="https://placehold.co/250x200"/>
             <span class="absolute top-0 m-2 p-2 inline-block w-20 h-8 text-center bg-green-500/90 rounded-full text-white font-bold text-xs"
             >25% OFF</span>
-            <div class="flex justify-between p-5">
+            <div class="flex justify-between p-5 items-center">
                 <div class="flex flex-col">
                     <span>Buy For</span>
-                    <span>1,000 EGP</span>
-                    <span>500 EGP</span>
+                    <span class="line-through">1,000 EGP</span>
+                    <span class="text-blue-800 font-bold text-2xl">500 EGP</span>
                 </div>
                 <button class="bg-blue-600 rounded-2xl h-[50px] w-[50px] font-bold text-white">+</button>
             </div>
--- app/components/home/ProductSection.vue
+++ app/components/home/ProductSection.vue
@@ -1,5 +1,14 @@
 <template>
-    <CardsProductCard/>
+    <p class="text-6xl font-bold">
+        Big Title Here
+    </p>
+    <span>Small Description There</span>
+    <div class="flex gap-2 mt-5">
+        <CardsProductCard/>
+        <CardsProductCard/>
+        <CardsProductCard/>
+        <CardsProductCard/>
+    </div>
 </template>
 
 <script setup>
