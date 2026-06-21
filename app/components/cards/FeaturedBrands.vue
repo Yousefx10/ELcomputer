947390165cab70767faa55e61b15deb54c259f15
Yousef Esbitan <yousef_espetan@yahoo.com>
2026-06-21 03:02:02 +0300

    inject example brands and stop on hover with NuxtLink

--- app/components/cards/FeaturedBrands.vue
+++ app/components/cards/FeaturedBrands.vue
@@ -8,14 +8,31 @@
         </div>
 
         <div class="overflow-hidden">
-                <div class="flex gap-5 animate-scroll">
-                    <div v-for="n in 12" :key="n"
-                        class="p-3 bg-white rounded-2xl border-2 border-gray-400 flex items-center justify-center">
+                <div class="flex gap-5 animate-scroll w-max">
 
-                        <img class="max-w-[150px] max-h-[150px] object-contain"
-                        src="https://placehold.co/150x150"/>
+                    <NuxtLink   v-for="brand in brands"
+                                :key="'A'+brand.id"
+                                :to="brand.link"
+                                class="min-w-[150px] h-[180px] p-3 bg-white rounded-2xl border-2 border-gray-400 flex items-center justify-center">
                         
-                    </div>
+                        <img class="max-w-[150px] max-h-[150px] object-contain"
+                        :src="brand.image"/>
+
+                    </NuxtLink>
+
+                    <!-- Second Dublicated Group, for seamless tranation -->
+                    <NuxtLink   v-for="brand in brands"
+                                :key="'B'+brand.id"
+                                :to="brand.link"
+                                class="min-w-[150px] h-[180px] p-3 bg-white rounded-2xl border-2 border-gray-400 flex items-center justify-center">
+                        
+                        <img class="max-w-[150px] max-h-[150px] object-contain"
+                        :src="brand.image"/>
+
+                    </NuxtLink>
+
+
+
                 </div>
         </div>
 
@@ -28,6 +45,52 @@ defineProps
     title:String,
     description:String,
 })
+
+
+
+//for testing purposes
+const brands =[
+    {
+        id:1,
+        image:'https://placehold.co/150x150',
+        link:'/'
+    },
+    {
+        id:2,
+        image:'https://placehold.co/150x150',
+        link:'/'
+    },
+    {
+        id:3,
+        image:'https://placehold.co/150x150',
+        link:'/'
+    },
+    {
+        id:4,
+        image:'https://placehold.co/150x150',
+        link:'/'
+    },
+    {
+        id:5,
+        image:'https://placehold.co/150x150',
+        link:'/'
+    },
+    {
+        id:6,
+        image:'https://placehold.co/150x150',
+        link:'/'
+    },
+    {
+        id:7,
+        image:'https://placehold.co/150x150',
+        link:'/'
+    },
+    {
+        id:8,
+        image:'https://placehold.co/150x150',
+        link:'/'
+    },
+]
 </script>
 
 <style scoped>
@@ -41,6 +104,10 @@ defineProps
 }
 
 .animate-scroll{
-        animation: scroll 30s linear infinite;
-    }
+    animation: scroll 30s linear infinite;
+}
+
+.animate-scroll:hover {
+    animation-play-state: paused;
+}
 </style>
