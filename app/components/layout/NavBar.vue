5df25b0dc079540153f5c1850649275b3fd52f8d
Yousef Esbitan <yousef_espetan@yahoo.com>
2026-06-22 02:38:49 +0300

    Responsive NavBar supporting phone and tablet drop menu

--- app/components/layout/NavBar.vue
+++ app/components/layout/NavBar.vue
@@ -1,25 +1,41 @@
 <template>
     <header class="bg-black text-white">
-        <nav class="flex justify-between items-center container mx-auto p-6">
+        <nav class="container mx-auto flex items-center justify-between px-4 py-6">
             <div>
                 <NuxtLink to="/">Elcomputer</NuxtLink>
             </div>
-            <ul class="flex gap-4">
+            <ul class="hidden lg:flex items-center gap-4 whitespace-nowrap">
                 <li><NuxtLink to="/">Home</NuxtLink></li>
                 <li><NuxtLink to="">Shop Category</NuxtLink></li>
                 <li><NuxtLink to="">Return Policy</NuxtLink></li>
                 <li><NuxtLink to="">Request Refund</NuxtLink></li>
                 <li><NuxtLink to="">Tool Kit</NuxtLink></li>
-                <li><input type="search" name="" id="" class="border rounded-full"></li>
+                <li><input type="search" class="w-56 border rounded-full px-4 py-1"></li>
                 <li><Icon name="lucide:user" size="24"/></li>
                 <li><Icon name="lucide:shopping-cart" size="24"/></li>
             </ul>
+            <button class="lg:hidden text-white" @click="isMenuOpen=!isMenuOpen">
+                <Icon name="lucide:menu" size="28"/>
+            </button>
         </nav>
+        <div v-if="isMenuOpen" class="lg:hidden px-6 pb-6 flex justify-center">
+            <ul class="grid grid-cols-2 gap-4 ">
+                <li><NuxtLink to="/">Home</NuxtLink></li>
+                <li><NuxtLink to="">Shop Category</NuxtLink></li>
+                <li><NuxtLink to="">Return Policy</NuxtLink></li>
+                <li><NuxtLink to="">Request Refund</NuxtLink></li>
+                <li><NuxtLink to="">Tool Kit</NuxtLink></li>
+                <li><input type="search" class="w-56 border rounded-full px-4 py-1"></li>
+                <li><Icon name="lucide:user" size="24"/></li>
+                <li><Icon name="lucide:shopping-cart" size="24"/></li>
+            </ul>
+        </div>
     </header>
 </template>
 
 
 <script setup>
+const isMenuOpen = ref(false)
 
 </script>
 
